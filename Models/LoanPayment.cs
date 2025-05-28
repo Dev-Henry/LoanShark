c06ab571b2f6c60dc1b197fa7c9da9f0aba5d73d
Dev-Henry <96271705+Dev-Henry@users.noreply.github.com>
2025-05-28 21:45:12 +0000

    Add project files.

---
+++ Models/LoanPayment.cs
@@ -0,0 +1,18 @@
+﻿namespace LoanShark.Models
+{
+    public class LoanPayment
+    {
+        public int Month { get; set; }
+
+        public double Payment { get; set; }
+
+        public double MonthlyPrincipal { get; set; }
+
+        public double MonthlyInterest { get; set; }
+
+        public double TotalInterest { get; set; }
+
+        public double Balance { get; set; }
+
+    }
+}
