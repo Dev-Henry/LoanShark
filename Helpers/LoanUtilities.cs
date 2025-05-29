504faf30e4378dfc1cdfb239e744ec2d5cfe2cc4
Dev-Henry <96271705+Dev-Henry@users.noreply.github.com>
2025-05-29 17:44:19 +0000

    final commit

---
+++ Helpers/LoanUtilities.cs
@@ -0,0 +1,84 @@
+﻿using LoanShark.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LoanShark.Helpers
+{
+    public static class LoanUtilities
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="loan"></param>
+        /// <returns>loan object</returns>
+        public static Loan GetPayment(Loan loan)
+        {
+            loan.Payments.Clear();
+
+
+            //Calculate the monthly payment
+            loan.Payment = CalculatePayment(loan.PurchaseAmount, loan.Rate, loan.Term);
+
+            var loanMonths = loan.Term * 12;
+
+            //variables to hold the total interest and balance
+            double balance = loan.PurchaseAmount;
+            double totalInterest = 0;
+            double monthlyPrincipal = 0;
+            double monthlyInterest = 0;
+            double monthlyRate = CalculateMonthlyRate(loan.Rate);
+
+            for (int month = 1; month <= loanMonths; month++)
+            {
+                monthlyInterest = CalculateMonthlyInterest(balance, monthlyRate);
+                totalInterest += monthlyInterest;
+                monthlyPrincipal = loan.Payment - monthlyInterest;
+                balance -= monthlyPrincipal;
+
+                LoanPayment loanPayment = new LoanPayment();
+
+                loanPayment.Month = month;
+                loanPayment.Payment = loan.Payment;
+                loanPayment.MonthlyPrincipal = monthlyPrincipal;
+                loanPayment.TotalInterest = totalInterest;
+                loanPayment.Balance = balance < 0 ? 0 : balance;
+
+                //Add the payment to the list
+                loan.Payments.Add(loanPayment);
+            };
+
+            loan.TotalInterest = totalInterest;
+            loan.TotalCost = loan.PurchaseAmount + totalInterest;
+
+            return loan;
+
+        }
+
+        /// <summary>
+        /// calculate the payment for a simple interest loan
+        /// </summary>
+        /// <param name="amount">Loan Amount</param>
+        /// <param name="rate">Annualized Loan Rate</param>
+        /// <param name="term">Term in years</param>
+        /// <returns>A monthly payment as a double </returns>
+
+        public static double CalculatePayment(double amount, double rate, double term)
+        {
+            var monthlyRate = CalculateMonthlyRate(rate);
+
+            var payment = (amount * monthlyRate) / (1 - Math.Pow(1 + monthlyRate, -term * 12));
+
+            return payment;
+        }
+
+        public static double CalculateMonthlyRate(double rate)
+        {
+            return rate / 1200;
+        }
+
+        public static double CalculateMonthlyInterest(double balance, double monthlyrate)
+        {
+            return balance * monthlyrate;
+        }
+
+    }
+}
--- Models/Loan.cs
+++ Models/Loan.cs
@@ -5,14 +5,14 @@ namespace LoanShark.Models
     public class Loan
     {
         [Required]
-        [Range(1, double.MaxValue, ErrorMessage = "Purchase amount must be greater than ₵1.")]
+        [Range(1, double.MaxValue, ErrorMessage = "Loan amount must be greater than ₵1.")]
         public double PurchaseAmount { get; set; }
 
         [Range(0.0, 100, MinimumIsExclusive = true, ErrorMessage = "Interest Range must be between 0 and 100")]
         public double Rate { get; set; }
 
         //Term is expressed in years 
-        [Range(1, 100, ErrorMessage = "Term must be between 1 and 100 years")]
+        [Range(1, 100, ErrorMessage = "Loan Term must be between 1 and 100 years")]
         public int Term { get; set; }
 
         public double Payment { get; set; }
